d7dea1e444064d05e9d0ae1e8c3b2d52612b58c9
Ali <alihuseyni523@gmail.com>
2025-06-27 19:46:21 +0300

    The database framework and the primary set of reference books

---
+++ CurrencyLog/src/test/java/com/myproject/currencylog/CurrencyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.currencylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
